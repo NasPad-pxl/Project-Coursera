3ad6def3ef25b32623dda6c7f0e156b9c53f584f
NasPad-pxl <mrafiqhikal4@gmail.com>
2025-12-24 14:08:14 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutUs.css'; // Assuming you have a CSS file for styling
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {/* <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity.</p> */}
+      <p className="about-us-content">
+        At Paradise Nursery, we provide the finest selection of indoor plants to bring nature into your home. Our team of experts is dedicated to ensuring you find the perfect plant for your space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
